c91a12082340ca1ba0000aaecd5f03668b84493c
Mesuteminkurt <mesut.kurtemin@gmail.com>
2026-08-10 03:13:58 +0300

    Revert C code changes, handle BMS SPI inversion in backend server.js

--- backend/server.js
+++ backend/server.js
@@ -154,7 +154,9 @@ if (MQTT_PASSWORD) {
                 } else if (key === 'e') {
                     currentData.energy = data[key];
                 } else if (key === 'spi') {
-                    currentData.bms_spi = data[key];
+                    // C kodunda (RxData[0] == 1) ? 0 : 1 şeklinde terslendiği için,
+                    // burada tekrar tersleyerek kartın asıl gönderdiği değere (0 = OK, 1 = Error) döndürüyoruz.
+                    currentData.bms_spi = data[key] == 1 ? 0 : 1;
                 } else if (key === 'mc') {
                     currentData.motor_contact = data[key];
                 } else if (key === 'tt') {
